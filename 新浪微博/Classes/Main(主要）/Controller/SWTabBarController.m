ec3404f50657c2378bb6fefed27c2ba7105d26c4
nitxc <xc@xcdeMacBook-Pro.local>
2015-03-05 15:05:37 +0800

    新浪微博环境基本搭建-01

---
+++ 新浪微博/Classes/Main(主要）/Controller/SWTabBarController.m
@@ -0,0 +1,29 @@
+//
+//  SWTabBarController.m
+//  新浪微博
+//
+//  Created by xc on 15/3/5.
+//  Copyright (c) 2015年 xc. All rights reserved.
+//
+
+#import "SWTabBarController.h"
+
+@interface SWTabBarController ()
+
+@end
+
+@implementation SWTabBarController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
