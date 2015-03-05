236394edaaa6daea70290a2d14dda90707cd4b50
nitxc <xc@xcdeMacBook-Pro.local>
2015-03-05 15:16:02 +0800

    新浪微博环境搭建-03

---
+++ 新浪微博/Classes/Main(主要）/Other/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  新浪微博
+//
+//  Created by xc on 15/3/5.
+//  Copyright (c) 2015年 xc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ 新浪微博/Classes/Main(主要）/Other/AppDelegate.m
@@ -0,0 +1,62 @@
+//
+//  AppDelegate.m
+//  新浪微博
+//
+//  Created by xc on 15/3/5.
+//  Copyright (c) 2015年 xc. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "SWTabBarController.h"
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    application.statusBarHidden = NO;//启动成功以后不隐藏状态栏
+    
+    // 1.创建窗口
+    self.window = [[UIWindow alloc] init];
+    self.window.frame = [UIScreen mainScreen].bounds;
+    
+    // 2.设置窗口的根控制器
+    SWTabBarController *tabbarVc = [[SWTabBarController alloc] init];
+    self.window.rootViewController = tabbarVc;
+    
+    
+    // 3.显示窗口(成为主窗口)
+    [self.window makeKeyAndVisible];
+    
+    
+    
+    
+    return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+@end
