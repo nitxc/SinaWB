887a330039851f083ca8d4ed5bb23588ea7919c1
nitxc <xc@xcdeMacBook-Pro.local>
2015-03-13 10:35:21 +0800

    控制器消息

---
+++ 新浪微博/Classes/Message(消息)/Controller/SWMessageViewController.m
@@ -0,0 +1,37 @@
+//
+//  SWMessageViewController.m
+//  新浪微博
+//
+//  Created by xc on 15/3/5.
+//  Copyright (c) 2015年 xc. All rights reserved.
+//
+
+#import "SWMessageViewController.h"
+
+@interface SWMessageViewController ()
+
+@end
+
+@implementation SWMessageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
